146ca4fe5a16f11a58e1d46d3aa36592d5ca4866
frfp <frfp@lyh.com>
2021-12-31 03:25:05 -0800

    complete login successfully

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const ejs = require('ejs')
 const app = express()
 const http = require('http')
 const fs = require('fs')
+const {Console} = require('console')
 const mongoose = require('mongoose');
-
+var router = express.Router();
 mongoose.connect('mongodb://172.21.2.236:27017/190110910119');
 // var db=mongoose.createConnection('mongodb://localhost/wuhu');
 const schema = {
@@ -41,7 +42,109 @@ const schema_m = {
 }
 const man = mongoose.model('manager', schema_m);
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: false }))
 app.use('/',express.static('public'))
+
+
+app.get('/LoginAction', function (req, res, next) {
+    username=req.query.username
+    password=req.query.password
+    userid = req.query.userid
+   
+ 
+
+      if(userid=="manager"){
+        man.find({manname:username},(err, data)=>{
+          if(data.length!=0){
+            if(data[0].manname==username&&data[0].manpwd==password){
+              ejs.renderFile('./public/index_m.html', {mname:username},  function(err, str){
+                // str => 输出渲染后的 HTML 字符串
+                if(err){console.log("File is not error.")}
+                else{
+                  res.statusCode=200
+                  res.setHeader('Content-type', 'text/html');
+                  res.end(str);
+                
+                }
+              });
+            }else{
+              ejs.renderFile('./public/index.html', {result:'1'},  function(err, str){
+                // str => 输出渲染后的 HTML 字符串
+                if(err){console.log("File is not error.")}
+                else{
+                  res.statusCode=200
+                  res.setHeader('Content-type', 'text/html');
+                  res.end(str);
+                
+                }
+              });
+            }
+
+          }else{
+            ejs.renderFile('./public/index.html', {result:'1'},  function(err, str){
+              // str => 输出渲染后的 HTML 字符串
+              if(err){console.log("File is not error.")}
+              else{
+                res.statusCode=200
+                res.setHeader('Content-type', 'text/html');
+                res.end(str);
+              
+              }
+            });
+          }
+        })
+
+         
+        
+      }
+         else{
+          mydata.find({uname:username},(err, data)=>{
+            if(data.length!=0){
+              if(data[0].uname==username&&data[0].upwd==password){
+                ejs.renderFile('./public/index_s.html', {sname: username},  function(err, str){
+                  // str => 输出渲染后的 HTML 字符串
+                  if(err){console.log("File is not error.")}
+                  else{
+                    res.statusCode=200
+                    res.setHeader('Content-type', 'text/html');
+                    res.end(str);
+                  
+                  }
+                });
+              }else{
+                ejs.renderFile('./public/index.html', {result:'1'},  function(err, str){
+                  // str => 输出渲染后的 HTML 字符串
+                  if(err){console.log("File is not error.")}
+                  else{
+                    res.statusCode=200
+                    res.setHeader('Content-type', 'text/html');
+                    res.end(str);
+                  
+                  }
+                });
+              }
+
+            }else{
+              ejs.renderFile('./public/index.html', {result:'1'},  function(err, str){
+                // str => 输出渲染后的 HTML 字符串
+                if(err){console.log("File is not error.")}
+                else{
+                  res.statusCode=200
+                  res.setHeader('Content-type', 'text/html');
+                  res.end(str);
+                
+                }
+              });
+            }
+          })
+         
+        }
+
+    
+        
+    // next()
+  })
 
   app.get("/ReginAction", (req, res)=>{
     // res.send(req.query)
@@ -59,6 +162,7 @@ app.use('/',express.static('public'))
 })
 
   
+
 
 
 app.listen(10119)
